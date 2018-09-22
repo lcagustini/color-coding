3ec47a5a4129a5bba3cb5c32e81cb00d0d1438d2
lcagustini <torresmon235@gmail.com>
2018-09-22 12:35:36 -0300

    Adjusted color space

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use sdl2::keyboard::Keycode;
 use std::io::prelude::*;
 use std::env;
 
-const WINDOW_WIDTH: u32 = 1200;
-const WINDOW_HEIGHT: u32 = 1000;
+const WINDOW_WIDTH: u32 = 480;
+const WINDOW_HEIGHT: u32 = 480;
 const BG_COLOR: Color = Color{r: 255, g: 255, b: 255, a: 255};
 
 macro_rules! rect(($x:expr, $y:expr, $w:expr, $h:expr) => (sdl2::rect::Rect::new($x as i32, $y as i32, $w as u32, $h as u32)));
@@ -30,12 +30,11 @@ fn main() {
     let mut canvas = window.into_canvas().build().unwrap();
 
     let mut lines_str: Vec<String> = read_file(&args[1]).split("\n").map(|x| x.to_owned()).collect();
-    let mut lines: Vec<Vec<u8>> = Vec::new();
+    let mut lines: Vec<u8> = Vec::new();
 
     for _ in 0..lines_str.len() {
-        lines.push(lines_str.pop().unwrap().into_bytes());
+        lines.append(&mut lines_str.pop().unwrap().into_bytes());
     }
-    lines.reverse();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
@@ -52,24 +51,22 @@ fn main() {
         canvas.set_draw_color(BG_COLOR);
         canvas.clear();
         
-        for i in 0..lines.len() {
-            for j in 0..lines[i].len()/3 {
-                let r = match lines[i].get(j) {
-                    Some(n) => n + 20,
-                    None => 0
-                };
-                let g = match lines[i].get(j+1) {
-                    Some(n) => n + 20,
-                    None => 0
-                };
-                let b = match lines[i].get(j+2) {
-                    Some(n) => n + 20,
-                    None => 0
-                };
+        for i in 0..lines.len()/3 {
+            let r = match lines.get(i) {
+                Some(n) => (27*(n.clone() as u32)/10) - 86,
+                None => 0
+            };
+            let g = match lines.get(i+1) {
+                Some(n) => (27*(n.clone() as u32)/10) - 86,
+                None => 0
+            };
+            let b = match lines.get(i+2) {
+                Some(n) => (27*(n.clone() as u32)/10) - 86,
+                None => 0
+            };
 
-                canvas.set_draw_color(Color{r: r, g: g, b: b, a: 255});
-                let _ = canvas.fill_rect(rect![j*16, i*16, 16, 16]);
-            }
+            canvas.set_draw_color(Color{r: r as u8, g: g as u8, b: b as u8, a: 255});
+            let _ = canvas.fill_rect(rect![16*(i as u32 % (WINDOW_WIDTH/16)), 16*(i as u32 / (WINDOW_WIDTH/16)), 16, 16]);
         }
 
         std::thread::sleep(std::time::Duration::new(0, 1_000_000_000u32 / 60));
